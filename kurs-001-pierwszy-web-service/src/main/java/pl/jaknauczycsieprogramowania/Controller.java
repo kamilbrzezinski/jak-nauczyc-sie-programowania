16b8f9e9d9ea644b30d01fc4238f8b3d7a9965f3
Kamil Brzezinski <kamil.brzezinski@gmail.com>
2018-12-29 19:25:44 +0100

    Kurs 001 - pierwszy web service

---
+++ kurs-001-pierwszy-web-service/src/main/java/pl/jaknauczycsieprogramowania/Controller.java
@@ -0,0 +1,28 @@
+package pl.jaknauczycsieprogramowania;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping("/hello2")
+    @ResponseBody
+    public String hello2(@RequestParam(name="name", required = false) String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/hello3/{name}")
+    @ResponseBody
+    public String hello3(@PathVariable("name") String name) {
+        return "Hello3 " + name;
+    }
+}
